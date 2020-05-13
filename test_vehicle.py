89637a8ac7522c6ac142767d33aae706112f0077
JakPopielarz <popielarzjakub7@gmail.com>
2020-05-13 18:03:35 +0200

    Vehicle fixes and sample/simple simulation rebuild

--- test_vehicle.py
+++ test_vehicle.py
@@ -6,47 +6,97 @@ Created on Wed May 13 09:28:19 2020
 @author: jakub
 """
 
-import vehicle
 import time
+import vehicle
 
-if __name__ == "__main__":
+grid_size = [50, 4]
+lane_index = 0
+interval = 0.1
+# "right", "left", "up" or "down"
+# "up" and "down" require a 2d grid
+direc = "left"
+spawn_prob = 0.1
+
+
+def main():
+    grid = generate_horizontal(lane_index, direc)
+    while True:
+        print("\n\n\n\n\n\n\n\n\n\n")
+        print_grid(grid, direc)
+        time.sleep(interval)
+        grid = tick(grid, lane_index, direc)
+
+def generate_horizontal(lane_on, direc):
     grid = []
-    grid_len = 30
-    interval = 0.3
-    
-    # create random number of vehicles on the road
-    for i in range(grid_len):
-        if vehicle.random.random() < 0.15:
-            grid.append(vehicle.Vehicle(v_max=2,
-                                        direction="right", on_screen=[i,0]))
-        else:
-            grid.append(" ")
-        
-    # print the road
-    for cell in grid:
-        if cell == " ":
-            print(" ", end="")
-        else:
-            print("#", end="")
-            cell.moved = False
-            v = cell.velocity[0]
-    print("\n"+("-"*grid_len))
-    time.sleep(interval)
-    
-    for i in range(len(grid)*2):
-        for index in range(len(grid)):
-            if grid[index] != " " and (not grid[index].moved):
-                grid = grid[index].advance(grid, index)
-        # print the road
-        print("\n\n\n")
-        
+    for i in range(grid_size[1]):
+        grid.append([])
+        for j in range(grid_size[0]):
+            if i == lane_on and vehicle.random.random() < spawn_prob:
+                grid[lane_on].append(vehicle.Vehicle(v_max=2, direction=direc,
+                                                     on_screen=[lane_on, j]))
+            else:
+                grid[i].append(" ")
+    return grid
+
+def generate_vertical(lane_on, direc):
+    grid = []
+    for i in range(grid_size[1]):
+        grid.append([])
+        for j in range(grid_size[0]):
+            if j == lane_on and vehicle.random.random() < spawn_prob:
+                grid[i].append(vehicle.Vehicle(v_max=2, direction=direc,
+                                               on_screen=[i, lane_on]))
+            else:
+                grid[i].append(" ")
+    return grid
+
+def print_grid(grid, direction):
+    for i in range(grid_size[1]):
         v = 0
-        for cell in grid:
+        for cell in grid[i]:
             if cell == " ":
                 print(" ", end="")
             else:
                 print("#", end="")
                 cell.moved = False
-                v = max(v, cell.velocity[0])
+                if direction == "right":
+                    v = max(v, cell.velocity[0])
+                elif direction == "left":
+                    v = min(v, cell.velocity[0])
         print("||"+str(v))
-        time.sleep(interval)
+    print("\n"+("-"*grid_size[0]))
+
+def tick(grid, lane_on, direction):
+    if direction == "left" or direction == "right":
+        new_grid = tick_horizontal(grid, lane_on)
+    return new_grid
+
+def tick_horizontal(grid, lane_on):
+    new_grid = vehicle.copy.copy(grid)
+    for index in range(len(new_grid[lane_on])):
+        if new_grid[lane_on][index] != " " and (not new_grid[lane_on][index].moved):
+            new_grid = new_grid[lane_on][index].advance(new_grid, lane_on, index)
+    return new_grid
+
+if __name__ == "__main__":
+    main()
+#    while True:
+#        for index in range(len(grid[0])):
+#            if grid[0][index] != " " and (not grid[0][index].moved):
+#                grid = grid[0][index].advance(grid, 0, index)
+#        # print the road
+#        print("\n\n\n\n\n\n\n\n\n\n")
+#
+#        v = 0
+#        for cell in grid[0]:
+#            if cell == " ":
+#                print(" ", end="")
+#            else:
+#                print("#", end="")
+#                cell.moved = False
+#                if direc == "right":
+#                    v = max(v, cell.velocity[0])
+#                elif direc == "left":
+#                    v = min(v, cell.velocity[0])
+#        print("||"+str(v))
+#        time.sleep(interval)
--- vehicle.py
+++ vehicle.py
@@ -38,13 +38,11 @@ class Vehicle:
                  safe_distance=1, slowdown_probability=0.3,
                  on_screen=[0, 0]):
         if direction == "up":
-            v_max *= -1
             current[1] *= -1
             v_change = [0, v_change*-1]
         elif direction == "down":
             v_change = [0, v_change]
         elif direction == "left":
-            v_max *= -1
             current[0] *= -1
             v_change = [v_change*-1, 0]
         elif direction == "right":
@@ -57,50 +55,95 @@ class Vehicle:
         self.velocity_change = v_change
         self.slowdown_probability = slowdown_probability
         self.on_screen = on_screen
+        self.direction = direction
         self.moved = False
 
-    def advance(self, grid, index):
+    def advance(self, grid, index_x, index_y):
         '''Advance the simulation by one step'''
         self.speed_up()
-        vehicle_ahead = self.check_ahead(grid, index)
-        if vehicle_ahead != None:
+
+        vehicle_ahead = self.check_ahead(grid, index_x, index_y)
+        if vehicle_ahead is not None:
             self.keep_safe(vehicle_ahead)
+
         self.randomize()
-        new_grid = self.move(grid, index)
+        new_grid = self.move(grid, index_x, index_y)
         self.moved = True
         return new_grid
 
     def speed_up(self):
         '''Accelerate'''
-        if self.velocity[0] < self.max_velocity:
+        if abs(self.velocity[0]) < self.max_velocity:
             self.velocity[0] += self.velocity_change[0]
-        if self.velocity[1] < self.max_velocity:
+        if abs(self.velocity[1]) < self.max_velocity:
             self.velocity[1] += self.velocity_change[1]
 
-    def check_ahead(self, grid, index):
+    def check_ahead(self, grid, index_x, index_y):
         '''Find closest car ahead'''
-        if index + 1 >= len(grid):
-            index -= len(grid)
+        if self.direction == "right" and index_y + 1 >= len(grid[index_x]):
+            index_y -= len(grid[index_x])
 
-        border = index+1+self.velocity[0]
-        for i in range(index+1, border):
+        if self.direction == "down" and index_x + 1 >= len(grid[index_y]):
+            index_x -= len(grid[index_x])
+
+        if self.direction == "right" or self.direction == "left":
+            ahead = self.check_horizontal(grid, index_x, index_y)
+        elif self.direction == "up" or self.direction == "down":
+            ahead = self.check_vertical(grid, index_x, index_y)
+        return ahead
+
+    def check_horizontal(self, grid, index_x, index_y):
+        '''Check for a car ahead in x axis'''
+        border = index_y + 1 + self.velocity[0]
+        for i in range(index_y+1, border):
+            if i >= len(grid[index_x]):
+                j = i - len(grid[index_x])
+            else:
+                j = i
+            if grid[index_x][j] != " ":
+                return grid[index_x][j]
+        return None
+
+    def check_vertical(self, grid, index_x, index_y):
+        '''Check for a car ahead in y axis'''
+        border = index_x + 1 + self.velocity[1]
+        for i in range(index_x+1, border):
             if i >= len(grid):
                 j = i - len(grid)
             else:
                 j = i
-            if grid[j] != " ":
-                return grid[j]
-        return
+            if grid[j][index_y] != " ":
+                return grid[j][index_y]
+        return None
 
     def keep_safe(self, ahead):
         '''Slow down, if distance to vehicle ahead is not safe'''
-        dist = self.distance(ahead)
+        if self.direction == "right" or self.direction == "left":
+            self.keep_safe_horizontal(ahead)
+        elif self.direction == "up" or self.direction == "down":
+            self.keep_safe_vertical(ahead)
+
+    def keep_safe_horizontal(self, ahead):
+        '''Decrease horizontal velocity if it is not safe'''
+        dist = self.distance_horizontal(ahead)
         if dist < self.safe_distance + self.velocity[0]:
             self.velocity[0] = dist
 
-    def distance(self, vehicle):
-        '''Return distance between self and vehicle passed as argument'''
+    def distance_horizontal(self, vehicle):
+        '''Return distance in x axis between
+        self and vehicle passed as argument'''
         return abs(vehicle.on_screen[0] - self.on_screen[0])
+
+    def keep_safe_vertical(self, ahead):
+        '''Decrease vertical velocity if it is not safe'''
+        dist = self.distance_vertical(ahead)
+        if dist < self.safe_distance + self.velocity[1]:
+            self.velocity[1] = dist
+
+    def distance_vertical(self, vehicle):
+        '''Return distance in t axis between
+        self and vehicle passed as argument'''
+        return abs(vehicle.on_screen[1] - self.on_screen[1])
 
     def randomize(self):
         '''with set probability slow down by 1 step'''
@@ -108,17 +151,39 @@ class Vehicle:
         if rnd < 1 - self.slowdown_probability:
             return
 
-        changed = self.velocity[0] - self.velocity_change[0]
-        if self.velocity[0] * changed < 0:
-            self.velocity[0] = 0
-        else:
-            self.velocity[0] = changed
+        if self.direction == "right" or self.direction == "left":
+            self.slow_down(0)
+        elif self.direction == "up" or self.direction == "down":
+            self.slow_down(1)
 
-    def move(self, grid, index):
-        '''Move the vehicle -> In this class or on board?'''
-        new_grid = copy.copy(grid)
-        new_index = index + self.velocity[0]
-        if new_index >= len(grid):
-            new_index -= len(grid)
-        new_grid[index], new_grid[new_index] = new_grid[new_index], new_grid[index]
+    def slow_down(self, index):
+        '''Decrease vehicle's velocity in a proper axis
+            index=0 for horizontal slow down,
+            index=1 for vertical slow down'''
+        changed = self.velocity[index] - self.velocity_change[index]
+        if self.velocity[index] * changed < 0:
+            self.velocity[index] = 0
+        else:
+            self.velocity[index] = changed
+
+    def move(self, grid, index_x, index_y):
+        '''Move the vehicle and return a grid with it moved'''
+        if self.direction == "right" or self.direction == "left":
+            new_grid = self.move_horizontal(grid, index_x, index_y)
+        elif self.direction == "up" or self.direction == "down":
+            new_grid = self.move_vertical(grid, index_x, index_y)
         return new_grid
+
+    def move_horizontal(self, grid, index_x, index_y):
+        '''Move the vehicle in x axis'''
+        new_grid = copy.copy(grid)
+        new_index = index_y + self.velocity[0]
+        if new_index >= len(grid[index_x]):
+            new_index -= len(grid[index_x])
+        new_grid[index_x][index_y], new_grid[index_x][new_index] = \
+            new_grid[index_x][new_index], new_grid[index_x][index_y]
+        return new_grid
+
+    def move_vertical(self, grid, index_x, index_y):
+        '''Move the vehicle in y axis'''
+        return ''
