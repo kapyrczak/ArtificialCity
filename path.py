194ee10f7beb5e01f1f4b830b47a6498c10cb3dd
kapyrczak <49714397+kapyrczak@users.noreply.github.com>
2020-06-06 13:25:23 +0200

    Update path.py

--- path.py
+++ path.py
@@ -1,17 +1,19 @@
 import random
 import pedestrian
+import config
 
 class Path:
-    def __init__(self, number = 1, length = 20, pedestrians = [], increaseProbability = 0.2,
-        speedLimit = 2, ticksPerSecond=30):
+    def __init__(self, number = 1, length = 20, pedestrians = [], spawnProbability = config.pedestrian_spawn_prob,
+        speedLimit = 2, ticksPerSecond=config.tps):
         self.number = number
         self.length = length
         self.pedestrians = pedestrians
-        self.increaseProbability = increaseProbability
+        self.spawnProbability = spawnProbability
         self.speedLimit = speedLimit
         self.ticksPerSecond = ticksPerSecond
         self.vMax = 2
         self.startingVelocity = speedLimit
+        self.numberOfPedestrians = 0
 
 
     def newPedestrian(self, length = 1, width = 1, vChange = 0.1, velocity = 0, travelled = 0):
@@ -22,8 +24,9 @@ class Path:
         self.pedestrians.insert(0, newPedestrian)
 
     def addPedestrian(self):
-        if random.random() < self.increaseProbability:
+        if random.random() < self.spawnProbability:
             self.newPedestrian()
+        self.pedestrians.sort(key=lambda pedestrian: pedestrian.travelled)
 
     def removePedestrians(self):
         end = len(self.pedestrians)
@@ -32,7 +35,9 @@ class Path:
             if self.pedestrians[index].travelled > self.length:
                 self.pedestrians.pop(index)
                 index = index - 1
+                self.numberOfPedestrians += 1
             index = index + 1
+
 
     def movePedestrians(self):
        for pedestrian in self.pedestrians:
@@ -45,8 +50,12 @@ class Path:
 
     def update(self):
         self.movePedestrians()
-        #self.removePedestrians()
+        self.removePedestrians()
         self.addPedestrian()
+
+        v_m = 0
+        for pedestrian in self.pedestrians:
+            self.vMax = max(v_m, pedestrian.velocity)
 
 
 
